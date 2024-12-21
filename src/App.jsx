f7532e5d193c724cc6c013eaeeb31130eb22fdb8
Sujay Garain <sgtech.ex@gmail.com>
2024-12-21 11:52:05 +0530

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import MainPage from "./pages/MainPage";
+// import ProjectViewer from "./pages/ProjectViewer";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/main/:section?" element={<MainPage />} />
+//         <Route path="/main/project/:id" element={<ProjectViewer />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
@@ -10,6 +29,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/main/:section?" element={<MainPage />} />
         <Route path="/main/project/:id" element={<ProjectViewer />} />
+        {/* Fallback Route for 404 */}
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </Router>
   );
